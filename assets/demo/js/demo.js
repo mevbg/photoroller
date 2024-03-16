91cce456200e69d5dfa2495bca33055efe6e7530
Martin Metodiev <martin@mev.bg>
2024-03-16 19:16:20 +0200

    Remove assets from git repository

--- assets/demo/js/demo.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(t){"use strict";t.ajax({url:"assets/demo/demo.html",dataType:"html"}).done(function(o){t(".demo-holder .content").html(o).promise().done(function(){t("#photoroller").photoroller({startpoint:6}),t.photoroller({target:t("#photoroller2"),startpoint:5})})})});
